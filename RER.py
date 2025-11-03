59d122eb64b3e5bed9bf4c1fb1434df9cd8c419b
HOMME <lucas.thiolas@gmail.com>
2025-11-04 00:18:13 +0100

    avancee code

--- RER.py
+++ RER.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A473921%3A&LineRef=STIF%3ALine%3A%3AC01742%3A'
+gare = '41527'    #473921  41527
+url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A'+ gare +'%3A&LineRef=STIF%3ALine%3A%3AC01742%3A'
 
 token = ''
 
@@ -16,6 +17,16 @@ if response.status_code == 200:
     print("succes")
     
     ## Traitements ############
+    echantillon  = data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]
+    
+    cpt_paris = 0
+    cpt_cergy = 0
+    
+    for idx in echantillon:
+        # Torcy
+        if idx["MonitoringRef"]["value"] == 'STIF:StopPoint:Q:41527:' and cpt_paris <3 :
+            cpt_paris = cpt_paris + 1;
+            
     
     ###########################
     
