6014e07b140d60977914de336c2bc7750823bf58
LTLVDSML <lucas.thiolas@gmail.com>
2026-06-18 00:24:12 +0200

    ajout reception et interpretation trame sur carte ARDUINO

--- controleAffichageLED/controleAffichageLED.ino
+++ controleAffichageLED/controleAffichageLED.ino
@@ -1,6 +1,7 @@
 // Variables fonctionnement
-int delaiEntreAcquisition = 100;  //ms
-byte seuilUn              = 100;
+int delaiEnvoiBit         = 1000;  //ms
+int delaiDetection        = 100;   //ms
+int seuilUn               = 200;
 
 // Definition du pin de reception donnees
 #define analog      A0
@@ -8,101 +9,67 @@ byte seuilUn              = 100;
 // Variables
 byte cpt_zeros = 0;
 byte idx_trame = 1;
-byte trame[96];
-byte idx_donnees = 1;
-byte donnees[16];
-
+byte trame[43];
+int horairesParis[3];
+int horairesCergy[3];
 
 void setup() {
   // Frequence de lecture pour le debug
   Serial.begin(9600); 
-
 }
 
 void loop() {
-  // Intervalle de temps entre chaque acquisition
-  delay(delaiEntreAcquisition);
-  // Reception signal
-  byte entree = analogRead(analog);
-  Serial.println(entree);
+  // Detection ou non d'une trame
+  delay(delaiDetection);
+  int entree = analogRead(analog);
 
-  // Gestion reception de trame /////////////////////
-  // Detection debut de trame
+  // Si on detecte un signal on lance le processus
   if (entree > seuilUn){
-    trame[0] = 1;
-    idx_trame = 1;
-    cpt_zeros = 0;
-    
-    // Tant que la trame continue d'etre recue
-    while (cpt_zeros < 12){
-      // Reception signal
-      byte entree = analogRead(analog);
-      Serial.println(entree);
-  
-      // Si l'entree recoit un 1
+    // On se deplace au milieu de la reception du signal
+    delay(delaiEnvoiBit/2);
+    // Recuperation de la trame
+    for (byte idx = 0; idx < 43; idx = idx+1){
+      int entree = analogRead(analog);
       if (entree > seuilUn){
-        cpt_zeros = 0;
-        trame[idx_trame] = 1;
+        trame[idx] = 1;        
       }
-      
-      // Si l'entree recoit un 0
-      else{
-        cpt_zeros = cpt_zeros + 1;
-        trame[idx_trame] = 0;
+      else {
+        trame[idx] = 0;  
       }
-      // Incrementation index trame
-      idx_trame = idx_trame +1;
-      if (idx_trame >= 96) {
-        break;
-      }
-
-
-      // Intervalle de temps entre chaque acquisition
-      delay(delaiEntreAcquisition);
+      // DEBUG : affichage de la trame
+      Serial.print(trame[idx]);
+      // Attente du bit suivant
+      delay(delaiEnvoiBit);
     }
-    // Decryptage trame
-    idx_donnees = decryptageTrame(&trame[0], &donnees[0], idx_trame);
 
-    // Affichage donnees traduites
-    
-  }
+    // Conversion donnees trame en valeurs numeriques
+    horairesParis[0] = (((((trame[1]*2 + trame[2])*2 + trame[3])*2 + trame[4])*2 + trame[5])*2 + trame[6])*2 + trame[7];
+    horairesParis[1] = (((((trame[8]*2 + trame[9])*2 + trame[10])*2+ trame[11])*2+ trame[12])*2 + trame[13])*2 + trame[14];
+    horairesParis[2] = (((((trame[15]*2+ trame[16])*2 + trame[17])*2 + trame[18])*2+ trame[19])*2+ trame[20])*2 + trame[21];
 
-}
+    horairesCergy[0] = (((((trame[22]*2 + trame[23])*2 + trame[24])*2 + trame[25])*2+ trame[26])*2 + trame[27])*2+ trame[28];
+    horairesCergy[1] = (((((trame[29]*2 + trame[30])*2 + trame[31])*2 + trame[32])*2 + trame[33])*2 + trame[34])*2 + trame[35];
+    horairesCergy[2] = (((((trame[36]*2 + trame[37])*2 + trame[38])*2+ trame[39])*2 + trame[40])*2+ trame[41])*2 + trame[42];
 
-// FONCTIONS //////////////////////////////////////////
-// Fonction de dechiffrement de la trame
-byte decryptageTrame(byte* addrTrame, byte* addrDonnees, byte tailleTrame){
-
-  byte tailleDonnees = 0;
-  byte idxValeur = 1;
-  byte valeur[6];
-
-   // pour chaque bit de la trame
-  for (byte idxTrame = 0; idxTrame < tailleTrame; idxTrame++){
-    idxValeur = 1;
-    valeur[6] = {0};
-    // pour chaque valeur de la trame
-    while(idxValeur < 6){
-      valeur[idxValeur] = *(addrDonnees + idxTrame);
-      idxValeur = idxValeur + 1;
-      idxTrame = idxTrame + 1;
+    // DEBUG : affichage horaires
+    Serial.print("-");
+    Serial.print("Paris : ");
+    for (byte idx = 0; idx < 3; idx = idx+1){
+      Serial.print(horairesParis[idx]);
+      Serial.print(" ");
     }
-    *(addrDonnees + tailleDonnees) = binaireSix(&valeur[0]);
-    tailleDonnees = tailleDonnees + 1;
-  }
-  return tailleDonnees;
-}
+
+    Serial.print("Cergy : ");
+    for (byte idx = 0; idx < 3; idx = idx+1){
+      Serial.print(horairesCergy[idx]);
+      Serial.print(" ");
+    }
+
+    // DEBUG : passage a la ligne
+      Serial.println();
 
 
-// Fonction traduction 6bits -> byte
-byte binaireSix(byte* addrValeur){
-
-  byte valeur = 0;
-  
-  for(byte idxBit = 0; idxBit < 6; idxBit++){
-    valeur = valeur + *(addrValeur + idxBit) * 2 ^ idxBit;
   }
 
-  return valeur;
-  
 }
+
